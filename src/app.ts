9e7bda73da6cdfc3e5e5787c592b74489238dd0d
Siva Prasath <6sivaprasath@gmail.com>
2023-06-05 12:42:57 +0530

    to 1.40 min mongoose and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import NoteModel from "./models/note";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("hello, world"); 
+app.get("/", async (req,res,next) => {
+    try {
+        //throw Error("Dumb");
+        const notes = await NoteModel.find().exec();
+        res.status(200).json(notes);
+    } catch (error) {
+       next();
+    }
 });
+
+app.use((req,res,next)=>{
+    next(Error("End pint not found!"));
+});
+
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error:unknown,req:Request,res:Response,next:NextFunction)=>{
+    console.log(error);
+    let errorMessage="unknown error occured";
+    if(error instanceof Error) errorMessage=error.message;
+    res.status(500).json({error:errorMessage});
+});
+
+
+
+
 
 export default app;
